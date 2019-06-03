1495b12de8d9728ad8d27423ee3790b1a2ea17fc
Davis Sinyard <d.sinyard01@gmail.com>
2019-06-03 15:02:23 -0500

    Upload

---
+++ modules/Certificate/_posts/certificatePage.md
@@ -0,0 +1,5 @@
+---
+title: "Certificate"
+---
+
+<iframe width="640" height="360" src="{{site.baseurl}}/questionmaker/build/Cert.jpg" frameborder="0" allowfullscreen></iframe>
--- questionmaker/quiz.js
+++ questionmaker/quiz.js
@@ -1,9 +1,7 @@
 function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
-
     function showQuestions(questions, quizContainer) {
         var output = [];
         var answers;
-
         // for each question...
         for (var i = 0; i < questions.length; i++) {
 
@@ -66,15 +64,25 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton) 
                     '<div class="answers">' + answers.join('') + '</div>'
                 );
             }
+            // Exclusion for certificate
+            else if (questions[i].type == 'certificate') {
+                certPos = i;
+                output.push('<div class="question">' + questions[i].question + '</div>' +
+                            '<div class="answers">' + answers.join('') + '</div>');
+
+            }
+
+
+
         }
 
         // finally combine our output list into one string of html and put it on the page
         quizContainer.innerHTML = output.join('');
     }
-
+    // Saves the number correct for the cookie function
+    var percentageCookie = 0;
     // show the questions
     showQuestions(questions, quizContainer);
-
     function showResults(questions, quizContainer, resultsContainer) {
 
         // gather answer containers from our quiz
@@ -83,7 +91,6 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton) 
         // keep track of user's answers
         var userAnswer = '';
         var numCorrect = 0;
-
         // for each question...
         for (var i = 0; i < questions.length; i++) {
 
@@ -94,7 +101,7 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton) 
             if (userAnswer === questions[i].correctAnswer) {
                 // add to the number of correct answers
                 numCorrect++;
-
+                percentageCookie++;
                 // color the answers green
                 answerContainers[i].style.color = 'lightgreen';
             }
@@ -105,19 +112,36 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton) 
             }
         }
 
-        // show number of correct answers out of total
-        resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+        // Calculates percentage correct
+        percentageCookie = Math.floor((numCorrect / questions.length) * 100);
+        // show number of correct answers out of total (replaced with percentage)
+        resultsContainer.innerHTML = percentageCookie + '%';
     }
+    // Cookie read val
+    var cookieRead;
     // when user clicks submit, show results
     submitButton.onclick = function() {
         showResults(questions, quizContainer, resultsContainer);
+        // Creates cookie
+        // Cookie should be active for 1 h 55 m
+        createCookie(quizContainer, percentageCookie, .08);
+        // Searches for cookie
+        cookieRead = readCookie(quizContainer);
+        // Retrieves cookie
+        readCookiePrint.innerHTML = cookieRead;
+        // Resets num value
+        percentageCookie = 0;
     }
 }
-
+// Prints cookieRead
+var readCookiePrint = document.getElementById('readPrint');
+//
 var quizContainer = document.getElementById('quiz');
 var resultsContainer = document.getElementById('results');
 var submitButton = document.getElementById('submit');
-
+// Debugging use only
+// var cookiePrint = document.getElementById('print');
+//
 function show_selected(value) {
         if (typeof value == 'undefined'){
         console.log("Invalid quiz")
@@ -140,4 +164,28 @@ function get_selected() {
     show_selected(value);
 }
 
+// Creates cookies
+function createCookie(name, value, days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        var expires = "; expires=" + date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name + "=" + value + expires + "; path=/";
+    // Debugging use only
+    //cookiePrint.innerHTML = 'Print for Cookies!';
+}
+/* Debug Use Only
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+*/
 document.getElementById('selectbtn').addEventListener('click', get_selected);
